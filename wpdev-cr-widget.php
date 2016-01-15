f379fbaa630bc5385c367437bbe50f53119cfe49
TedSlaterLA <ted@libertyalliance.com>
2016-01-15 15:02:29 -0500

    created options page

--- wpdev-cr-widget.php
+++ wpdev-cr-widget.php
@@ -26,16 +26,74 @@ $myUpdateChecker = new $className(
 );
  
 /**********
+Create Options Page
+**********/
+
+add_action( 'admin_menu', 'crls_add_admin_menu' );
+add_action( 'admin_init', 'crls_settings_init' );
+
+function crls_add_admin_menu(  ) { 
+	add_options_page( 'wpdev-cr-widget', 'wpdev-cr-widget', 'manage_options', 'wpdev-cr-widget', 'wpdev-cr-widget_options_page' );
+}
+
+function crls_settings_init(  ) { 
+	register_setting( 'pluginPage', 'crls_settings' );
+	add_settings_section(
+		'crls_pluginPage_section', 
+		__( 'Your section description', 'wordpress' ), 
+		'crls_settings_section_callback', 
+		'pluginPage'
+	);
+
+	add_settings_field( 
+		'lsappid', 
+		__( 'Settings field description', 'wordpress' ), 
+		'lsappid_render', 
+		'pluginPage', 
+		'crls_pluginPage_section' 
+	);
+}
+
+function lsappid_render(  ) { 
+	$options = get_option( 'crls_settings' );
+	?>
+	<input type='text' name='crls_settings[lsappid]' value='<?php echo $options['lsappid']; ?>'>
+	<?php
+}
+
+function crls_settings_section_callback(  ) { 
+	echo __( 'This section description', 'wordpress' );
+}
+
+function crls_options_page(  ) { 
+
+	?>
+	<form action='options.php' method='post'>
+		
+		<h2>wpdev-cr-widget</h2>
+		
+		<?php
+		settings_fields( 'pluginPage' );
+		do_settings_sections( 'pluginPage' );
+		submit_button();
+		?>
+		
+	</form>
+	<?php
+
+}
+ 
+/**********
 Add Conservative Review Liberty Score Code to Footer
 **********/
 
-$lsappid = 'CRTV-LibertyAlliance';
+//$lsappid = 'CRTV-LibertyAlliance';
 
 function wpdev_crls_footer() {
     global $lsappid;
     if($lsappid) {
         echo '
-<script> var crApiKey = \'' . $lsappid . '\'</script> 
+<script> var crApiKey = \'' . $options['lsappid'] . '\'</script> 
 <script src="https://api.conservativereview.com/Scripts/CRWidget.js"></script>
 ';
     }
