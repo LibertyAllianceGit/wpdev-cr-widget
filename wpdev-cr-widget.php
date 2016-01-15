49bb7b90bcf8c189d324927a262cebe19ae49644
TylerJohnsonLA <tyler@libertyalliance.com>
2016-01-15 16:14:29 -0500

    Add Options Page

--- wpdev-cr-widget.php
+++ wpdev-cr-widget.php
@@ -26,6 +26,79 @@ $myUpdateChecker = new $className(
 );
 
 /**********
+Enqueue Admin Plugin Styles
+**********/
+function wpdev_cr_score_plugin_css() {
+        wp_register_style( 'wpdev-cr-score-plugin-css', plugin_dir_url(__FILE__) . 'cr-style.css' );
+        wp_enqueue_style( 'wpdev-cr-score-plugin-css' );
+}
+add_action('admin_enqueue_scripts', 'wpdev_cr_score_plugin_css', 20);
+
+/**********
+Setup Options Page
+**********/
+
+add_action( 'admin_menu', 'wpdevcrscore_add_admin_menu' );
+add_action( 'admin_init', 'wpdevcrscore_settings_init' );
+
+function wpdevcrscore_add_admin_menu() { 
+	add_submenu_page( 
+        'options-general.php', 
+        'WPDev CR Score', 
+        'WPDev CR Score', 
+        'manage_options', 
+        'wpdevcrscore', 
+        'wpdevcrscore_options_page' 
+    );
+}
+
+function wpdevcrscore_settings_init() { 
+	register_setting( 'pluginPage', 'wpdevcrscore_settings' );
+
+	add_settings_section(
+		'wpdevcrscore_pluginPage_section', 
+		__( 'Settings', 'wpdevcrscore' ), 
+		'wpdevcrscore_settings_section_callback', 
+		'pluginPage'
+	);
+
+	add_settings_field( 
+		'wpdevcrscore_appid', 
+		__( 'APP ID', 'wpdevcrscore' ), 
+		'wpdevcrscore_appid_render', 
+		'pluginPage', 
+		'wpdevcrscore_pluginPage_section' 
+	);
+}
+
+function wpdevcrscore_appid_render() { 
+	$options = get_option( 'wpdevcrscore_settings' );
+	?>
+	<input type='text' name='wpdevcrscore_settings[wpdevcrscore_appid]' placeholder='CRTV-...' value='<?php echo $options['wpdevcrscore_appid']; ?>'>
+	<?php
+}
+
+function wpdevcrscore_settings_section_callback() { 
+	echo __( 'Plugin to enable Conservative Review\'s Liberty Score to run on sites, with a [score][/score] shortcode.', 'wpdevcrscore' );
+}
+
+function wpdevcrscore_options_page() { 
+	?>
+<div class="wpdev-cr-score-settings">
+	<form action='options.php' method='post'>
+		<h2><img src="<?php echo plugin_dir_url(__FILE__) . 'wpdev-cr-score-widget-logo.png'; ?>" /></h2>
+		<hr/>
+		<?php
+		settings_fields( 'pluginPage' );
+		do_settings_sections( 'pluginPage' );
+		submit_button();
+		?>
+	</form>
+</div>
+	<?php
+}
+
+/**********
 Add Conservative Review Liberty Score Code to Footer
 **********/
 
